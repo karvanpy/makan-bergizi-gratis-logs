fd9c6a56ce72535aee5c825b38064922be2f4d29
karvanpy <ervankurniawan41@gmail.com>
2025-05-08 17:32:08 +0700

    Add MBG-002

--- src/data/cases.ts
+++ src/data/cases.ts
@@ -11,11 +11,27 @@ export const cases: Case[] = [
   	"date": "2025-05-07",
   	"category": ["keracunan", "sekolah", "program pemerintah"],
   	"sources": [
-    	"https://news.detik.com/berita/d-7904664/puluhan-pelajar-di-bogor-alami-keracunan-diduga-usai-komsumsi-mbg"
+    	"https://news.detik.com/berita/d-7904664/puluhan-pelajar-di-bogor-alami-keracunan-diduga-usai-komsumsi-mbg",
+      "https://www.liputan6.com/news/read/6016388/puluhan-siswa-dan-guru-bina-insani-kota-bogor-keracunan-menu-mbg"
   	],
   	"description": "Dinas Kesehatan Kota Bogor melaporkan adanya dugaan keracunan makanan setelah konsumsi makanan dari program Makan Bergizi Gratis (MBG) di lingkungan sekolah. Sebanyak 36 pelajar mengalami keluhan seperti diare, mual, muntah, dan demam. 12 orang sempat dirawat di rumah sakit, dan 5 di antaranya masih menjalani perawatan.",
   	"severity": "medium"
-	}
+	},
+  {
+    "id": "mbg-002",
+    "title": "Puluhan Siswa dari Lima Sekolah di PALI Sumsel Diduga Keracunan MBG",
+    "location": {
+      "city": "Talang Ubi",
+      "province": "Sumatera Selatan"
+    },
+    "date": "2025-05-05",
+    "category": ["keracunan", "sekolah", "program pemerintah"],
+    "sources": [
+      "https://www.kompas.id/artikel/puluhan-siswa-dari-lima-sekolah-di-pali-sumsel-diduga-keracunan-mbg"
+    ],
+    "description": "Sebanyak 64 siswa dari lima sekolah di Kecamatan Talang Ubi, Kabupaten Penukal Abab Lematang Ilir (PALI), Sumatera Selatan, diduga mengalami keracunan makanan setelah mengonsumsi menu Makan Bergizi Gratis (MBG). Para siswa dirawat di IGD RSUD Talang Ubi dengan gejala sakit perut, pusing, dan mual. Sampel menu MBG berupa ikan tongkol suwir telah diambil untuk diuji laboratorium oleh Dinas Kesehatan dan Kepolisian Resor PALI.",
+    "severity": "high"
+  }
 ];
 
 export const getCategories = (): string[] => {
